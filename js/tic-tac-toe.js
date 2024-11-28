b5a3504dcf3407f21463943a94211b848c782207
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2024-11-28 17:50:14 +1100

    working in console, hard coded input currently, haven't really put code into objest

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,56 +1,167 @@
-function Gameboard () {
+//function Gameboard () {
   /*let boardPos = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
   ]*/
   
-  function initPosition () {
-    player=0;
-    sate=0;
-    return{player, state}
+function makeBoard() {
+  let board = [];
+  let player = 0;
+  let state = null;
+  for (let i = 0 ; i < 9; i++)
+  {
+    board[i] = {player, state};
   }
+  // console.log(board);
+  // board[2].player = "John";
+  // board[2].state = "X";
+  // console.log(board);
+  return board;
+}
 
-  let pos1 = initPosition;
-  let pos2 = initPosition;
-  let pos3 = initPosition;
-  let pos4 = initPosition;
-  let pos5 = initPosition;
-  let pos6 = initPosition;
-  let pos7 = initPosition;
-  let pos8 = initPosition;
-  let pos9 = initPosition;
+function makePlayer(name, state) {
+  return {name,state};
+}
 
-  board = {pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9}
+function playerMove(playerIn,position,madeboardIn)
+{
+  madeboardIn[position].player = playerIn.name;
+  madeboardIn[position].state = playerIn.state;
+  console.log(madeboardIn);
 
-  const updateBoard = (player, state, position) => {
-    let x = pos + position; 
-    boardPos.x.player=player;
-    boardPos.x.sate=state;
+}
+
+function checkGameState(madeboardIn)
+{
+
+  console.log("madeboardIn[0].state"+madeboardIn[0].state);
+  console.log("madeboardIn[3].state"+madeboardIn[3].state);
+  console.log("madeboardIn[6].state"+madeboardIn[6].state);
+  
+  if ( //((madeboardIn[0].state === madeboardIn[1].state === madeboardIn[2].state)) //&& (madeboardIn[0].state !== 0))
+      //|| ((madeboardIn[3].state === madeboardIn[4].state === madeboardIn[5].state)) //&& (madeboardIn[3].state !== 0))
+      //|| ((madeboardIn[6].state === madeboardIn[7].state === madeboardIn[8].state)) //&& (madeboardIn[6].state !== 0))
+      //|| ((madeboardIn[0].state === madeboardIn[4].state === madeboardIn[8].state)) //&& (madeboardIn[0].state !== 0))
+      //|| ((madeboardIn[3].state === madeboardIn[5].state === madeboardIn[6].state)) //&& (madeboardIn[3].state !== 0))
+       //((madeboardIn[0].state === madeboardIn[3].state)) // === madeboardIn[6].state)) //&& (madeboardIn[0].state !== 0))
+       ((madeboardIn[0].state === madeboardIn[3].state) && (madeboardIn[0].state === madeboardIn[6].state) && madeboardIn[0].state !== null)
+      //|| ((madeboardIn[1].state === madeboardIn[4].state === madeboardIn[7].state)) //&& (madeboardIn[1].state !== 0))
+      //|| ((madeboardIn[2].state === madeboardIn[5].state === madeboardIn[8].state)) //&& (madeboardIn[2].state !== 0))
+    )
+  {
+    console.log("game over");
   }
-  return {boardPos, newPostion};
-}
-
-function positionState (player, state) {
-  retun {player, state};
-}
-
-function Player (name) {
-  return {name};
-}
-
-function GameTurn (playerOne, playerTwo) {
-  turn = playerOne;
-  const turnToggle = () => {
-    if(turn === playerOne) turn = playerTwo;
+  else
+  {
+    console.log("keep playing");
   }
-
+  
 }
 
-function getPosition () {
-  console.log("Enter position");
+const player1 = makePlayer("John","X");
+const player2 = makePlayer("Fred","O");
 
-}
+const madeboard = makeBoard();
+// console.log(madeboard);
+// madeboard[2].player = "John";
+// madeboard[2].state = "X";
+// console.log(madeboard);
+
+playerMove(player1,0,madeboard);
+console.log(madeboard);
+checkGameState(madeboard);
+playerMove(player2,8,madeboard);
+console.log(madeboard);
+checkGameState(madeboard);
+playerMove(player1,2,madeboard);
+console.log(madeboard);
+checkGameState(madeboard);
+playerMove(player2,1,madeboard);
+console.log(madeboard);
+checkGameState(madeboard);
+playerMove(player1,6,madeboard);
+console.log(madeboard);
+checkGameState(madeboard);
+playerMove(player2,4,madeboard);
+console.log(madeboard);
+checkGameState(madeboard);
+playerMove(player1,3,madeboard);
+console.log(madeboard);
+checkGameState(madeboard);
+
+
+
+
+
+
+
+  
+
+  // function initPosition () {
+  //   player=0;
+  //   state=0;
+  //   return {player, state};
+  // }
+
+  // let pos1 = initPosition();
+  // let pos2 = initPosition();
+  // let pos3 = initPosition();
+  // let pos4 = initPosition();
+  // let pos5 = initPosition();
+  // let pos6 = initPosition();
+  // let pos7 = initPosition();
+  // let pos8 = initPosition();
+  // let pos9 = initPosition();
+
+  
+
+  // board = {pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9}
+
+  // pos1.player = "John";
+  // pos1.state = "X";
+  
+  // console.log(board.pos1);
+  // console.log(board.pos1.player);
+  // console.log(board.pos1.state);
+
+  // board.pos2.player = "Fred";
+  // board.pos2.state = "O";
+
+  // console.log(board.pos2);
+  // console.log(board.pos2.player);
+  // console.log(board.pos2.state);
+
+
+
+//   const updateBoard = (player, state, position) => {
+//     let x = pos + position; 
+//     boardPos.x.player=player;
+//     boardPos.x.sate=state;
+//   }
+//   return {boardPos, newPostion};
+// }
+
+// function positionState (player, state) {
+//   retun {player, state};
+// }
+
+// function Player (name) {
+//   return {name};
+// }
+
+// function GameTurn (playerOne, playerTwo) {
+//   turn = playerOne;
+//   const turnToggle = () => {
+//     if(turn === playerOne) turn = playerTwo;
+//   }
+
+// }
+
+// function getPosition () {
+//   console.log("Enter position");
+
+// }
 
 
 
